7bf0ccbf88fd8d57c4165377e2ab07ffc4a8f484
Sara Gold <sara.gold@outlook.com>
2018-02-15 16:21:52 -0200

    Bloqueando usuários já cadastrados - Part I
    Blocking users already registred.

--- registra_usuario.php
+++ registra_usuario.php
@@ -13,6 +13,44 @@
 	//A variavel recebe o retorno da função
 	$link = $objeto_db -> conecta_mysql();
 
+//Verificando se o usuário já existe
+	$sql = "select * from usuarios where usuario = '$usuario' ";
+
+	if($resultado_id = mysqli_query($link, $sql)){
+
+		$dados_usuarios = mysqli_fetch_array($resultado_id);
+
+		if(isset($dados_usuarios['usuario'])){
+
+			echo 'Usuário existente!';
+		}else{
+
+			echo 'Usuário pode ser cadastrado!';
+		}
+	}else{
+		echo 'Erro ao tentar localizar registro';
+	}
+
+	//Verificando de Email já existe
+	$sql = "select * from usuarios where email = '$email' ";
+
+	if($resultado_id = mysqli_query($link, $sql)){
+
+		$dados_usuarios = mysqli_fetch_array($resultado_id);
+
+		if(isset($dados_usuarios['email'])){
+
+			echo 'E-mail existente!';
+		}else{
+
+			echo 'E-mail pode ser cadastrado!';
+		}
+	}else{
+		echo 'Erro ao tentar localizar registro';
+	}
+
+	die();
+
 	//Query de insert
 	$sql = "insert into usuarios(usuario, email, senha) values ('$usuario', '$email', '$senha')";
 
